2b5b573f47142ca063a34b349465fb617c688d52
KaytoChan <elizabeth.flores204314@potros.itson.edu.mx>
2022-03-21 22:09:23 -0700

    Rutas agregadas en la app.js Listas

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const categoriasRoute = require('./routes/categoriasRoute')
 const productosRoute = require('./routes/productosRoute')
 const pagosRoute = require('./routes/pagosRoute')
+const clientesRoute = require('./routes/clientesRoute')
+const destinosRoute = require('./routes/destinosRoute')
+const pedidosRoute = require('./routes/pedidosRoute')
 
 const app = express()
 
@@ -12,6 +15,12 @@ app.use('/categorias', categoriasRoute)
 app.use('/productos', productosRoute)
 
 app.use('/pagos', pagosRoute)
+
+app.use('/clientes', clientesRoute)
+
+app.use('/destinos', destinosRoute)
+
+app.use('/pedidos', pedidosRoute)
 
 app.post('*', (req, res) => {
   res.status(404).send(`<h1>No se pudo encontrar ${req.originalUrl}</h1>`)
